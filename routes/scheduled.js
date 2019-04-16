a3a649fa9b21e164a2afd64de03ea4cc74029345
nicoto <toulousenicholas@gmail.com>
2019-04-15 19:28:08 -0700

    scheduled tours route

--- routes/scheduled.js
+++ routes/scheduled.js
@@ -2,11 +2,11 @@ var router = require('express').Router();
 var connection = require('../config/connection')
 
 router.get('/:tour_id', function (req, res) {
-
-    connection.query('SELECT * FROM scheduled_tours WHERE id=?', [req.param.tour_id], function (error, results, fields) {
-        console.log(results);
+    console.log(" req.query.tour_id is ", req.params.tour_id);
+    connection.query('SELECT scheduled, cancellation, firstname, lastname, email, walktitle, neighborhood, description, bio, mediawikiURL FROM scheduled_tours AS st INNER JOIN users AS u INNER JOIN tours AS t WHERE st.id=? AND st.user_id = u.id AND st.tour_id = t.id', [req.params.tour_id], function (error, results, fields) {
         if (error) res.send(error)
         else {
+            console.log("results ", results);
             res.render("pages/scheduled", {scheduled: results});
         }
     });
