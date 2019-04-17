30b1b16929a99ef61a73cd6e563d8af80116a80c
Sandy <sandycnavarro@gmail.com>
2019-04-16 19:49:10 -0700

    signup and sign in edited and routed

--- routes/users.js
+++ routes/users.js
@@ -4,19 +4,49 @@
 // var app = require('express')();
 
 var router = require('express').Router()
+var connection = require('../config/connection')
+var express = require('express');
 
-router.get('/signup', function(req, res, next){
+router.use(express.urlencoded({ extended: true }));
+
+router.get('/signup', function(req, res){
     // res.send("/users"); // goes to signin.ejs
     // sign them in somehow
     console.log("sign them up or log them in first, then go to the scheduled tour to register that user.")
-    next();
     res.render("pages/signup");
 }); 
 
-router.get('/signup/:tour_id', function(req, res){
-    console.log(req.params.tour_id);
-    res.send("/users"); // goes to signin.ejs
+router.post('/signup', function(req, res){
+    // res.send("/users"); // goes to signin.ejs
+    // sign them in somehow
+    console.log("sign them up or log them in first, then go to the scheduled tour to register that user.")
+    console.log(req.body)
+    // connection.query('INSERT INTO users(firstname, lastname, mobilephone, email, password, category) VALUES (?)', [req.body.firstname,req.body,lastname,req.body.mobilephone,req.body.email,req.body.password,'user'], function(error, results, fields){
+    connection.query('INSERT INTO users SET ?', {...req.body, category: 'user'}, function(error,result){
+    if (error) 
+            res.send(error)
+        else 
+            res.redirect("/users/signin")
+    }); 
 }); 
+
+router.get('/signin', function(req, res){
+    // res.send("/users"); // goes to signin.ejs
+    // sign them in somehow
+    console.log("sign them up or log them in first, then go to the scheduled tour to register that user.")
+    res.render("pages/signin");
+}); 
+router.post('/signin', function(req, res){
+    // res.send("/users"); // goes to signin.ejs
+    // sign them in somehow
+    console.log("sign them up or log them in first, then go to the scheduled tour to register that user.")
+    res.render("pages/signin");
+}); 
+
+// router.get('/signup/:tour_id', function(req, res){
+//     console.log(req.params.tour_id);
+//     res.send("/users"); // goes to signin.ejs
+// }); 
 
 router.get('/', function(req, res){
     res.send("/users"); // goes to signin.ejs
